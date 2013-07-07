f25c06329bd7dc0abb06ea94880ec8139a2228e9
riking <rikingcoding@gmail.com>
2013-07-07 13:28:19 -0700

    Move methods down to make javadoc look better

--- src/main/java/me/riking/templateworlds/api/TemplateWorlds.java
+++ src/main/java/me/riking/templateworlds/api/TemplateWorlds.java
@@ -9,6 +9,32 @@ public class TemplateWorlds extends JavaPlugin {
     private ApiMain api;
     private static TemplateWorlds instance;
 
+    /**
+     * Get the instance of the plugin.
+     * @return the plugin instance, or null if disabled
+     */
+    public static TemplateWorlds getInstance() {
+        return instance;
+    }
+
+    /**
+     * Get the ApiMain class, which is used for all operations.
+     *
+     * @return the ApiMain
+     */
+    public ApiMain getApi() {
+        return api;
+    }
+
+    /**
+     * Get the ApiMain class, which is used for all operations.
+     *
+     * @return the ApiMain
+     */
+    public static ApiMain getApiStatic() {
+        return instance.api;
+    }
+
     @Override
     public void onLoad() {
     }
@@ -30,35 +56,5 @@ public class TemplateWorlds extends JavaPlugin {
     public void onDisable() {
         api.onDisable();
         instance = null;
-    }
-
-    /**
-     * Get the instance of the plugin.
-     * @return the plugin instance, or null if disabled
-     */
-    public static TemplateWorlds getInstance() {
-        return instance;
-    }
-
-    /**
-     * Get the ApiMain class, which is used for all operations.
-     * <p>
-     * Please don't try to instanceof the ApiMain, you shouldn't need to.
-     *
-     * @return the ApiMain
-     */
-    public ApiMain getApi() {
-        return api;
-    }
-
-    /**
-     * Get the ApiMain class, which is used for all operations.
-     * <p>
-     * Please don't try to instanceof the ApiMain, you shouldn't need to.
-     *
-     * @return the ApiMain
-     */
-    public static ApiMain getApiStatic() {
-        return instance.api;
     }
 }
