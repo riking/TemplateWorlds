6b42538896463bf672e3d216928d33eb701afc9c
riking <rikingcoding@gmail.com>
2013-11-03 11:54:53 -0800

    Add support for 1.6.4

---
+++ cb1_6_R3/src/main/java/com/github/riking/templateworlds/impl/craftbukkit1_6_R3/BiomedVoidGenerator.java
@@ -0,0 +1,46 @@
+package com.github.riking.templateworlds.impl.craftbukkit1_6_R3;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+
+import org.bukkit.ChunkSnapshot;
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
+
+import com.github.riking.templateworlds.impl.common.TemplatedGenerator;
+
+public class BiomedVoidGenerator extends TemplatedGenerator {
+    private List<BlockPopulator> blockPopulators;
+
+    public BiomedVoidGenerator(World world) {
+        super(world);
+        blockPopulators = new ArrayList<BlockPopulator>(1);
+        blockPopulators.add(new OBCTemplateBlockPopulator(world));
+    }
+
+    public BiomedVoidGenerator(World world, Collection<BlockPopulator> extraPops) {
+        this(world);
+        blockPopulators.addAll(extraPops);
+    }
+
+    @Override
+    public List<BlockPopulator> getDefaultPopulators(World target) {
+        return blockPopulators;
+    }
+
+    public short[][] generateExtBlockSections(World world, Random random, int x, int z, BiomeGrid biomes) {
+        ChunkSnapshot templateChunk = templateWorld.getChunkAt(x, z).getChunkSnapshot(true, true, false);
+        for (int px = 0; px < 16; px++) {
+            for (int pz = 0; pz < 16; pz++) {
+                biomes.setBiome(px, pz, templateChunk.getBiome(px, pz));
+            }
+        }
+        short[][] ret = new short[256 / 16][];
+        for (int i = 0; i < 16; ret[i++] = new short[4096])
+            ;
+        return ret;
+    }
+}
---
+++ cb1_6_R3/src/main/java/com/github/riking/templateworlds/impl/craftbukkit1_6_R3/CBApiMain.java
@@ -0,0 +1,59 @@
+package com.github.riking.templateworlds.impl.craftbukkit1_6_R3;
+
+import java.util.Collection;
+
+import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.craftbukkit.v1_6_R3.util.Versioning;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+
+import com.github.riking.templateworlds.impl.common.BaseApiMain;
+import com.github.riking.templateworlds.impl.common.TemplatedGenerator;
+
+public class CBApiMain extends BaseApiMain {
+    public CBApiMain() {
+        // CB Version check
+        Versioning.getBukkitVersion();
+    }
+
+    @Override
+    public ChunkGenerator getTemplatedGenerator(String world, String template) {
+        World temp = Bukkit.getWorld(template);
+        Validate.notNull(temp, "The right-hand side of the chunk generator string for TemplateWorlds must be a valid world to use as a template");
+        return new BiomedVoidGenerator(temp);
+    }
+
+    @Override
+    public World createWorld(String name, World template) {
+        Validate.notNull(template, "Template world cannot be null");
+
+        World w = new WorldCreator(name).copy(template).generateStructures(false).generator(new BiomedVoidGenerator(template)).createWorld();
+        w.setAutoSave(false);
+        return w;
+    }
+
+    @Override
+    public World createWorld(String name, World template, Collection<BlockPopulator> extraPopulators) {
+        Validate.notNull(template, "Template world cannot be null");
+        Validate.allElementsOfType(extraPopulators, BlockPopulator.class, "All extra populators must be BlockPopulators");
+
+        World w = new WorldCreator(name).copy(template).generateStructures(false).generator(new BiomedVoidGenerator(template, extraPopulators)).createWorld();
+        w.setAutoSave(false);
+        return w;
+    }
+
+    @Override
+    public void changeTemplate(World templated, World newTemplate) {
+        validateTemplatedWorld(templated);
+        TemplatedGenerator gen = (TemplatedGenerator) templated.getGenerator();
+        BlockPopulator pop1 = templated.getPopulators().get(0);
+
+        gen.templateWorld = newTemplate;
+        if (pop1 instanceof OBCTemplateBlockPopulator) {
+            ((OBCTemplateBlockPopulator) pop1).templateWorld = newTemplate;
+        }
+    }
+}
