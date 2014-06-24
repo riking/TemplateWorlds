098cf26e9bdbea50fde505410159dcb3c443ef1c
Kane York <rikingcoding@gmail.com>
2014-06-24 13:15:33 -0700

    add @deprecated

--- common/src/main/java/com/github/riking/templateworlds/impl/common/VoidGenerator.java
+++ common/src/main/java/com/github/riking/templateworlds/impl/common/VoidGenerator.java
@@ -14,6 +14,7 @@ public class VoidGenerator extends ChunkGenerator {
     }
 
     @Override
+    @Deprecated
     public short[][] generateExtBlockSections(World contextWorld, Random worldRandom, int cx, int cz, BiomeGrid biomes) {
         return new short[0][];
     }
